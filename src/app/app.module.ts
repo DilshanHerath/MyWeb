15977067eb17c12b9e2c55109dd8e41d4b6cc18f
Dilshan <dilshanmadushanka981@gmail.com>
2021-05-12 22:58:42 +0530

    Assignment Finish

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './component/home/home.component';
 import {NgxSpinnerModule} from 'ngx-spinner';
+import { AssignmentComponent } from './component/assignment/assignment.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    AssignmentComponent
   ],
   imports: [
     BrowserModule,
-    NgxSpinnerModule
+    NgxSpinnerModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
